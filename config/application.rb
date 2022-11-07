212bf150a96f87cdff817ab0411d9fbc02a8f4cc
Manuel Ramos <manuelramoslg@gmail.com>
2022-11-07 12:08:57 +0100

    Added i18n.rails
    Fixes #2

--- config/application.rb
+++ config/application.rb
@@ -28,10 +28,12 @@ module InspiringTest
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Madrid"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+
   end
 end
